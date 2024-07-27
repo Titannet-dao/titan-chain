98171e112e26c773a72c7effcc26694d416070cc
nezha <nezhayoung@outlook.com>
2024-07-27 18:58:20 +0800

    fix

--- scripts/update.sh
+++ scripts/update.sh
@@ -16,19 +16,11 @@ rsync -av --exclude "data" ~/titan_bak/* ~/.titan
 # shellcheck disable=SC2046
 rm $(which titand)
 
-# Download new executable program
-wget -P ~/. https://github.com/Titannet-dao/titan-chain/releases/download/v2.0.0/titan_chain_v2.0.0_linux_amd.tar.gz
-# Unzip the executable program into the global directory
-tar -zxvf ~/titan_chain_v2.0.0_linux_amd.tar.gz  --strip-components 1 -C /usr/local/bin
-# Delete downloaded packages
-rm ~/titan_chain_v2.0.0_linux_amd.tar.gz
-
 # Download new genesis file
 wget -P ~/. https://raw.githubusercontent.com/Titannet-dao/titan-chain/main/genesis/genesis.json
 
 # Replace new genesis file
 mv ~/genesis.json ~/.titan/config/genesis.json
-
 
 mkdir ~/.titan/data
 # Build data/priv_validator_state.json 文件
@@ -57,5 +49,3 @@ node = "tcp://localhost:26657"
 # Transaction broadcasting mode (sync|async)
 broadcast-mode = "sync"' > ~/.titan/config/client.toml
 
-# Start a new titan program
-systemctl start titan
